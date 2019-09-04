7ed0bf0b83b0a9eef5f908918a7f44e0693d4916
Decopied <salmanalimd123@gmail.com>
2019-09-04 17:53:23 +0530

    meow

--- app.py
+++ app.py
@@ -12,10 +12,6 @@ app.secret_key = 'noob'
 api = Api(app)
 
 
-@app.before_first_request
-def create_table():
-    db.create_all()
-
 
 jwt = JWT(app,authenticate,identity)
 api.add_resource(Store,'/store/<string:name>')
